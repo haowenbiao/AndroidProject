5be2dec3fdf246eb2619d123acd740dc5ee52422
haowenbiao <haowenbiao@126.com>
2017-03-29 21:51:54 +0800

    20170329-001

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonOfShowJumpBetweenActivities =(Button) findViewById(R.id.showJumpBetweenActivities);
+        buttonOfShowJumpBetweenActivities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,JumpBetweenActivities.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override

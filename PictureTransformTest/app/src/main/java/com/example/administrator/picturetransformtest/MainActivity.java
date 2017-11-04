866951d8c18c912b142f59594ac6b651ecde95ba
haowenbiao <haowenbiao@126.com>
2017-11-04 23:33:29 +0800

    20171104

--- PictureTransformTest/app/src/main/java/com/example/administrator/picturetransformtest/MainActivity.java
+++ PictureTransformTest/app/src/main/java/com/example/administrator/picturetransformtest/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.administrator.picturetransformtest;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Camera;
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
